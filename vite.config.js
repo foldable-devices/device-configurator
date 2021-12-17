15612c4c0dd6bdec50ece2f1631a28af0a797654
Alexis Menard <alexis.menard@intel.com>
2021-12-17 13:47:23 -0500

    Reorganized directory structure to better match vitejs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/device-configurator/',
-  publicDir: 'demo',
   build: {
     outDir: 'docs/',
     emptyOutDir: true,
